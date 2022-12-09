7dc08f70865240d5621589982089bcb8ae461112
Dmitry Savostyanov <dokken671@gmail.com>
2022-12-10 00:38:16 +0700

    add new tests

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -37,4 +37,69 @@ public class CardDeliveryTest {
         $(".notification").shouldBe(visible, Duration.ofSeconds(15));
         $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + dayMeeting(3)));
     }
+    @Test
+    void shouldTestFieldCity() {
+        $("[data-test-id=city] input").setValue("Стрежевой").pressEnter();
+        $("[data-test-id=date] input").doubleClick().sendKeys(dayMeeting(3));
+        $("[data-test-id=name] input").setValue("Савостьянов Дмитрий");
+        $("[data-test-id=phone] input").setValue("+79131041698");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldTestFieldData() {
+        $("[data-test-id=city] input").setValue("Томск").pressEnter();
+        $("[data-test-id=date] input").doubleClick().sendKeys(dayMeeting(0));
+        $("[data-test-id=name] input").setValue("Савостьянов Дмитрий");
+        $("[data-test-id=phone] input").setValue("+79131041698");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $(".input_invalid .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
+    }
+
+    @Test
+    void shouldTestFieldName() {
+        $("[data-test-id=city] input").setValue("Томск").pressEnter();
+        $("[data-test-id=date] input").doubleClick().sendKeys(dayMeeting(3));
+        $("[data-test-id=name] input").setValue("Dmitry Savostianov");
+        $("[data-test-id=phone] input").setValue("+79131041698");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldTestFieldPhone() {
+        $("[data-test-id=city] input").setValue("Томск").pressEnter();
+        $("[data-test-id=date] input").doubleClick().sendKeys(dayMeeting(3));
+        $("[data-test-id=name] input").setValue("Савостьянов Дмитрий");
+        $("[data-test-id=phone] input").setValue("89130000000");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldTestFieldAgreement() {
+        $("[data-test-id=city] input").setValue("Томск").pressEnter();
+        $("[data-test-id=date] input").doubleClick().sendKeys(dayMeeting(3));
+        $("[data-test-id=name] input").setValue("Савостьянов Дмитрий");
+        $("[data-test-id=phone] input").setValue("+79130000000");
+        $(withText("Забронировать")).click();
+        $("[data-test-id='agreement'].input_invalid ").shouldBe(visible);
+        $(".input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+    }
+
+    @Test
+    void shouldTestFieldNonInputPhone() {
+        $("[data-test-id=city] input").setValue("Томск").pressEnter();
+        $("[data-test-id=date] input").doubleClick().sendKeys(dayMeeting(3));
+        $("[data-test-id=name] input").setValue("Савостьянов Дмитрий");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
 }
